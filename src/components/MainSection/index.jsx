f7d1fa55e2e88d7ae2cfa206e4a61fdaa330ac66
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-02-26 14:28:38 +0100

    add Projects component and integrate it into MainSection for enhanced project display

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -1,9 +1,11 @@
 import About from "../About";
+import Projects from "../Projects";
 
 function MainSection() {
   return (
-    <div className="bg-slate-700 h-screen flex flex-col justify-center items-center">
+    <div className="bg-slate-700 h-screen flex flex-col justify-center items-center gap-4">
       <About />
+      <Projects />
     </div>
   );
 }
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,1 +1,17 @@
-function Projects() 
+function Projects() {
+  return (
+    <section
+      id="projects"
+      className="bg-slate-800 text-white py-10 rounded-2xl"
+    >
+      <div className="container mx-auto px-4 ">
+        <h2 className="text-3xl font-bold text-center mb-8">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="bg-slate-700 rounded-2xl p-4">asfasf</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
