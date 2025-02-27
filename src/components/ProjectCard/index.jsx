fec51d73a254035ed41e111d763e542a59e4e4ff
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-02-27 13:52:56 +0100

    add ProjectCard component and integrate it into Projects for improved project display

---
+++ src/components/ProjectCard/index.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+
+Projectcard.propTypes = {
+  project: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  liveUrl: PropTypes.string.isRequired,
+  repoUrl: PropTypes.string.isRequired,
+};
+
+function Projectcard({ project, image, description, liveUrl, repoUrl }) {
+  return (
+    <div>
+      <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
+        <img src={image} className="w-full h-40 bg-red-500 rounded-lg mb-4" />
+        <h3 className="text-xl font-bold mb-2">{project}</h3>
+        <p className="text-sm">{description}</p>
+        <div className="mt-4 flex justify-evenly w-full">
+          <a
+            href={liveUrl}
+            target="blank"
+            className="text-gray-100 cursor-pointer capitalize"
+          >
+            live link
+          </a>
+          <a
+            href={repoUrl}
+            target="blank"
+            className="text-gray-100 cursor-pointer capitalize"
+          >
+            Repo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projectcard;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,3 +1,6 @@
+import Projectcard from "../ProjectCard";
+import biddable from "../../assets/SP2Screen.png";
+
 function Projects() {
   return (
     <section
@@ -7,45 +10,14 @@ function Projects() {
       <div className="container mx-auto px-4 ">
         <h2 className="text-3xl font-bold text-center mb-8">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
-            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
-            <h3 className="text-xl font-bold mb-2">Project 1</h3>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              euismod bibendum laoreet.
-            </p>
-            <div className="mt-4 flex justify-evenly w-full">
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-            </div>
-          </div>
-          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
-            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
-            <h3 className="text-xl font-bold mb-2">Project 1</h3>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              euismod bibendum laoreet.
-            </p>
-            <div className="mt-4 flex justify-evenly w-full">
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-            </div>
-          </div>
-          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
-            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
-            <h3 className="text-xl font-bold mb-2">Project 1</h3>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              euismod bibendum laoreet.
-            </p>
-            <div className="mt-4 flex justify-evenly w-full">
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-              <a className="text-gray-100 cursor-pointer">Repo</a>
-            </div>
-          </div>
+          <Projectcard
+            image={biddable}
+            project="Biddable."
+            description="Biddable. was made as to answer the task of creating a auction website for our semester project"
+            liveUrl={"https://glennsp2.netlify.app/"}
+          />
+          <Projectcard />
+          <Projectcard />
         </div>
       </div>
     </section>
