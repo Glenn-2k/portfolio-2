ca1c8e65bada8224c4f8241110ed873a88f13e78
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-02-27 13:21:09 +0100

    enhance Projects component layout with improved card design and hover effects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,7 +7,35 @@ function Projects() {
       <div className="container mx-auto px-4 ">
         <h2 className="text-3xl font-bold text-center mb-8">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-slate-700 rounded-2xl p-4">asfasf</div>
+          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
+            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
+            <h3 className="text-xl font-bold mb-2">Project 1</h3>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              euismod bibendum laoreet.
+            </p>
+            <div className="mt-4 flex justify-evenly w-full">
+              <a className="text-gray-100 cursor-pointer">Repo</a>
+              <a className="text-gray-100 cursor-pointer">Repo</a>
+              <a className="text-gray-100 cursor-pointer">Repo</a>
+            </div>
+          </div>
+          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
+            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
+            <h3 className="text-xl font-bold mb-2">Project 1</h3>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              euismod bibendum laoreet.
+            </p>
+          </div>
+          <div className="hover:shadow-2xl transition ease-in-out rounded-2xl p-6 flex flex-col items-center text-center">
+            <div className="w-full h-40 bg-red-500 rounded-lg mb-4"></div>
+            <h3 className="text-xl font-bold mb-2">Project 1</h3>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              euismod bibendum laoreet.
+            </p>
+          </div>
         </div>
       </div>
     </section>
