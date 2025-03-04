d4ff3eeffa9db9bed2bd9833c49c879becbd65c7
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-03-04 11:08:42 +0100

    enhance Logo component with hover effects for improved interactivity

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
       <img
         src={GlennLogo}
         alt="logo"
-        className="h-8 w-auto m-4 pl-6 cursor-pointer"
+        className="h-8 w-auto m-4 pl-6 cursor-pointer hover:skew-2 hover:scale-105 transform transition duration-500 ease-in-out"
       />
     </div>
   );
